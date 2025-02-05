e3b49e43ad635d407ca2308e05d96f3d908dea29
Sumit Das <davsumit@amazon.com>
2025-02-05 19:25:43 +0530

    added authentication

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,17 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { Amplify } from 'aws-amplify'
+import config from './aws-exports'
+import '@aws-amplify/ui-react/styles.css'
+import { ThemeProvider } from '@aws-amplify/ui-react'
+
+Amplify.configure(config)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </StrictMode>,
 )
